5cd62e793916ca957c07d57f4f0695da2dd15d8d
LeoLunardon <115712958+LeoLunardon@users.noreply.github.com>
2023-11-01 17:47:04 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
 import HomePage from "./Pages/HomePage.jsx";
 import AnimePage from "./Pages/AnimePage.jsx";
