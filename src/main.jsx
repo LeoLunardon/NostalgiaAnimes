0819ab354f1ecfd8035499df865db50207c999c8
LeoLunardon <115712958+LeoLunardon@users.noreply.github.com>
2023-11-01 17:45:53 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <RouterProvider router={router}> </RouterProvider>
   </React.StrictMode>
